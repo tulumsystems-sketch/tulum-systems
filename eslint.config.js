9a45b525addd9c176fa8733e417b4dc080b336fe
tulumsystems-sketch <tulumsystems@gmail.com>
2026-06-12 17:05:19 -0300

    feat: new landing

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
 import js from "@eslint/js";
 import astro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
 
 export default [
   {
     ignores: ["dist/**", ".astro/**", "node_modules/**"],
   },
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   ...astro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    rules: {
+      "astro/no-set-html-directive": "error",
+    },
+  },
 ];
